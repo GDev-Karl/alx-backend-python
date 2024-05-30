bbe2dfd0e82ca8f2c40aabce49840d05b360cbf4
Karl <tekamkarl@gmail.com>
2024-05-30 15:45:13 +0100

    Implement the TestMemoize(unittest.TestCase) class with a test_memoize method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -42,27 +42,26 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(url), payload)
 
 
-class TestClass:
-
-    def a_method(self):
-        return 42
-
-    @memoize
-    def a_property(self):
-        return self.a_method()
-
-
 class TestMemoize(unittest.TestCase):
 
-    @patch.object(TestClass, "a_method", return_value=42)
-    def test_memoize(self, mock_a_method):
+    def test_memoize(self):
         """Test the memoize function."""
-        my_object = TestClass()
+        class TestClass:
 
-        self.assertEqual(my_object.a_property, 42)
-        self.assertEqual(my_object.a_property, 42)
+            def a_method(self):
+                return 42
 
-        mock_a_method.assert_called_once()
+            @memoize
+            def a_property(self):
+                return self.a_method()
+    
+        with patch.object(TestClass, "a_method", return_value=42) as mock_a_method:
+            my_object = TestClass()
+
+            self.assertEqual(my_object.a_property, 42)
+            self.assertEqual(my_object.a_property, 42)
+
+            mock_a_method.assert_called_once()
 
 
 if __name__ == "__main__":
