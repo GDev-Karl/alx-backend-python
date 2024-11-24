4a5379595f2e5e8b229a81d4f83f9c1bceb7ee26
Karl <tekamkarl@gmail.com>
2024-11-24 05:09:14 +0100

    return 10 random numbers

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" a python module to loop 10 times """
+"""
+A python module to returns 10 random numbers
+using async comprehension
+"""
 import random
 import asyncio
 from typing import Generator
@@ -7,7 +10,7 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    async_generator - function to loop 10 times
+    async_generator - function to return 10 random numbers
     Arguments:
         no arguments
     Returns:
@@ -16,4 +19,3 @@ async def async_generator() -> Generator[float, None, None]:
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        
